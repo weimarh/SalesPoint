de3a5c24a00a7e187cbca5c12201d0a712dded5f
weimarbarea <ggmwba@gmail.com>
2025-04-01 11:36:15 -0700

    Add unit tests project and initial test cases
    - Created a new "Tests" project in the solution with Debug and Release configurations.
    - Added global using directives for Moq, FluentAssertions, and MediatR.
    - Updated MSTestSettings to enable parallel test execution.
    - Defined the `Tests.csproj` file targeting .NET 8.0 with necessary package references.
    - Implemented `CreateCategoryCommandHandlerTests` class with tests for various scenarios.

---
+++ Tests/GlobalUsings.cs
@@ -0,0 +1,5 @@
+﻿global using Moq;
+global using FluentAssertions;
+global using ErrorOr;
+global using MediatR;
+global using Domain.DomainErrors;
---
+++ Tests/UnitTests/ApplicationUnitTests/Categories/Create/CreateCategoryCommandHandlerTests.cs
@@ -0,0 +1,86 @@
+﻿using Application.UseCases.Categories.Create;
+using Domain.Entities.Categories;
+using Domain.Primitives;
+
+namespace Tests.UnitTests.ApplicationUnitTests.Categories.Create
+{
+    [TestClass]
+    public class CreateCategoryCommandHandlerTests
+    {
+        [TestMethod]
+        public async Task HandleCreateCategory_WhenInputIsCorrect_ShouldReturnSuccess()
+        {
+            var mockCategoryRepository = new Mock<ICategoryRepository>();
+            var mockUnitOfWork = new Mock<IUnitOfWork>();
+            var handler = new CreateCategoryCommandHandler(mockUnitOfWork.Object, mockCategoryRepository.Object);
+
+            //Arrange
+            CreateCategoryCommand command = new CreateCategoryCommand
+            (
+                "CategoryName",
+                "CategoryDescription"
+            );
+
+            //Act
+            var result = await handler.Handle(command, default);
+
+            //Assert
+            result.IsError.Should().BeFalse();
+            result.Value.Should().BeOfType<Unit>();
+            mockCategoryRepository.Setup(repo => repo.AddAsync(
+                It.Is<Category>(category =>
+                    category.CategoryName == command.CategoryName &&
+                    category.Description == command.Description
+            ))).Returns(Task.CompletedTask);
+        }
+
+        [TestMethod]
+        public async Task HandleCreateCategory_WhenCategoryNameIsEmptyString_ShouldReturnValidationError()
+        {
+            var mockCategoryRepository = new Mock<ICategoryRepository>();
+            var mockUnitOfWork = new Mock<IUnitOfWork>();
+            var handler = new CreateCategoryCommandHandler(mockUnitOfWork.Object, mockCategoryRepository.Object);
+
+            //Arrange
+            CreateCategoryCommand command = new CreateCategoryCommand
+            (
+                "",
+                "CategoryDescription"
+            );
+
+            //Act
+            var result = await handler.Handle(command, default);
+
+            //Assert
+            result.IsError.Should().BeTrue();
+            result.FirstError.Type.Should().Be(ErrorType.Validation);
+            result.FirstError.Code.Should().Be(CategoryErrors.EmptyName.Code);
+            result.FirstError.Description.Should().Be(CategoryErrors.EmptyName.Description);
+        }
+
+        [TestMethod]
+        public async Task HandleCreateCategory_WhenCategoryDescriptionIsTooLong_ShouldReturnValidationError()
+        {
+            var mockCategoryRepository = new Mock<ICategoryRepository>();
+            var mockUnitOfWork = new Mock<IUnitOfWork>();
+            var handler = new CreateCategoryCommandHandler(mockUnitOfWork.Object, mockCategoryRepository.Object);
+
+            //Arrange
+            CreateCategoryCommand command = new CreateCategoryCommand
+            (
+                "CategoryName",
+                "CategoryDescriptionCategoryDescriptionCategoryDescriptionCategoryDescriptionCategoryDescriptionCategoryDescription" +
+                "CategoryDescriptionCategoryDescriptionCategoryDescriptionCategoryDescriptionCategoryDescriptionCategoryDescription"
+            );
+
+            //Act
+            var result = await handler.Handle(command, default);
+
+            //Assert
+            result.IsError.Should().BeTrue();
+            result.FirstError.Type.Should().Be(ErrorType.Validation);
+            result.FirstError.Code.Should().Be(CategoryErrors.DescriptionTooLong.Code);
+            result.FirstError.Description.Should().Be(CategoryErrors.DescriptionTooLong.Description);
+        }
+    }
+}
